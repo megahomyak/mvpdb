be1c3b13f70060dedf0b9399a031ec71d4ad7a17
megahomyak <g.megahomyak@gmail.com>
2025-06-10 19:55:43 +0500

    I think I've described the structures and I can start adding writes/reads, but tsserver is being a bug here, doesn't derive types. I'll try to fix that too

--- db.js
+++ db.js
@@ -1,30 +1,75 @@
 let makeMVPDB = bitStorage => {
-    /*UNLIKELY:
-    let pointer = pointee => ({
-        size: 50,
-        make: addr => pointee(addr),
-    });*/
-
     let union = variantProducer => addr => {
         let maxSize = 0;
         let contents = variantProducer(variant => {
-
+            let { size, contents } = variant(addr);
+            if (size > maxSize) {
+                maxSize = size;
+            }
+            return contents;
         });
         return {
             size: maxSize,
-            make: () => contents,
+            contents,
         };
     };
 
-    let slot = () => union(variant => ({
+    let struct = fieldProducer => addr => {
+        let totalSize = 0;
+        let contents = fieldProducer(field => {
+            let { size, contents } = field(addr + totalSize);
+            totalSize += size;
+            return contents;
+        });
+        return {
+            size: totalSize,
+            contents,
+        };
+    };
+
+    let bool = () => addr => ({
+        size: 1,
+        contents: { addr },
+    });
+
+    let pointer = () => addr => ({
+        size: 50,
+        contents: { addr },
+    });
+
+    let array = (itemsCount, item) => addr => {
+        let contents = [];
+        let totalSize = 0;
+        for (let i = 0; i < itemsCount; ++i) {
+            let itemDescription = item(addr + totalSize);
+            totalSize += itemDescription.size;
+            contents.push(itemDescription.contents);
+        }
+        return contents;
+    };
+
+    let enum_ = variantProducer => addr => {
+        let variantsCount = 0;
+        let variants = variantProducer(() => {
+            let oldVariantsCount = variantsCount;
+            ++variantsCount;
+            return oldVariantsCount;
+        });
+        return {
+            size: Math.ceil(Math.log2(variantsCount)),
+            contents: { addr, variants },
+        };
+    };
+
+    let slotModel = union(variant => ({
         data: variant(struct(field => ({
-            branches: field(array(pointer(slot()))),
+            branches: field(array(2, pointer())),
         }))),
         free: variant(struct(field => ({
-            next: field(pointer(slot())),
+            next: field(pointer()),
         }))),
         transaction: variant(struct(field => ({
-            next: field(pointer(slot())),
+            next: field(pointer()),
             data: field(enum_(variant => ({
                 bit0: variant(),
                 bit1: variant(),
@@ -33,15 +78,16 @@ let makeMVPDB = bitStorage => {
             }))),
         }))),
     }));
-    let staticState = struct(field => ({
+    let staticStateModel = struct(field => ({
         activeVariantIndex: field(bool()),
-        variants: field(array(struct(field => ({
-            dataTreeRoot: field(pointer(slot())),
-            freesQueueBeginning: field(pointer(slot())),
-            eof: field(pointer(slot())),
-            transactionQueueBeginning: field(pointer(slot())),
+        variants: field(array(2, struct(field => ({
+            dataTreeRoot: field(pointer()),
+            freesQueueBeginning: field(pointer()),
+            eof: field(pointer()),
+            transactionQueueBeginning: field(pointer()),
         })))),
-    })).make(0);
+    }));
+    staticStateModel(0).contents // tsserver can't resolve, ugh
 
     return {
         get: () => {},
