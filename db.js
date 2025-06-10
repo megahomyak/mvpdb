28622a636db8d382fccddba88cce1e2601b17f24
megahomyak <g.megahomyak@gmail.com>
2025-06-10 16:47:28 +0500

    Trying to do "clean unions", but these can't be adequately clean, so I'm scrapping that idea

--- db.js
+++ db.js
@@ -1,4 +1,33 @@
 let makeMVPDB = bitStorage => {
+    let makeStruct = initializer => baseAddr => initializer((() => {
+        return fieldSize => {
+            let oldTotalBias = baseAddr;
+            baseAddr += fieldSize;
+            return () => oldTotalBias;
+        };
+    })());
+
+    let dataSlot = () => array(pointer(dataSlot()));
+    let freeSlot = () => struct(field => ({
+        next: field(pointer(freeSlot())),
+    }));
+    let transactionSlot = () => struct(field => ({
+        next: field(pointer(freeSlot())),
+        data: field(enum_(variant => ({
+            bit0: variant(),
+            bit1: variant(),
+            removal: variant(),
+            insertion: variant(),
+        }))),
+    }));
+    let staticState = struct(field => ({
+        activeVariantIndex: field(bool()),
+        variants: field(array(struct(field => ({
+            dataTree: field(pointer())
+        })))),
+    }));
+
+    // ---------
     let { read, write, commit } = bitStorage;
     const ptrSize = 50;
     const staticStateValidityFlagPtrPtr = 0;
